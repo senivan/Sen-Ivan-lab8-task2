0dd7df0ac7fcec57a61056345e900e5acc4b4ca4
Ivan Sen <senivan1997@gmail.com>
2023-11-03 10:14:02 +0000

    Add check horizontal function

--- puzzle.py
+++ puzzle.py
@@ -10,6 +10,28 @@ def check_horizontal(board:list[str])->bool:
             return False
     return True
 
+def check_vertical(board:list[str])->bool:
+    '''
+    Function that checks vertical lines of board
+    >>> board = [\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"]
+    >>> check_vertical(board)
+    False
+    '''
+    for i in range(len(board)):
+        line = [board[j][i] for j in range(len(board))]
+        if len(set(line)) != len(line):
+            return False
+    return True
+
 def validate_board(board:list[str]) -> bool:
     '''
     Function that validates board
