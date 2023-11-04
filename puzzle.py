ac7cf3d9915ba32f3e687deb83f777de537c27ce
Ivan Sen <senivan1997@gmail.com>
2023-11-04 17:29:46 +0000

    check_color function done

--- puzzle.py
+++ puzzle.py
@@ -45,6 +45,45 @@ def check_vertical(board:list[str])->bool:
                 return False
     return True
 
+def check_color(board:list[str]) -> bool:
+    '''
+    Function that checks color lines of board
+    >>> board = [\
+ "**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"]
+    >>> check_color(board) 
+    True
+    '''
+    board_rotate = []
+    for i in range(len(board)):
+        line = []
+        for j in range(len(board)):
+            line.append(board[j][i])
+        board_rotate.append(line)
+    
+    test_lines = []
+    hor_index = 8
+    ver_index = 0
+    for i in range(6):
+        line = []
+        line += ''.join(board_rotate[ver_index][:len(board)-i]) + ''.join(board[hor_index][i+1:])
+        ver_index+=1
+        hor_index-=1
+        test_lines.append(line)
+
+    for line in test_lines:
+        for lt in line:
+            if lt != '*' and lt != ' ' and line.count(lt) > 1:
+                return False
+    return True
+
 def validate_board(board:list[str]) -> bool:
     '''
     Function that validates board
